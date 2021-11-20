9d5aaed57f584468d3f1d0e510d29a82585b854b
Anton Matusenco <anton@matusenco.com>
2021-11-20 14:14:33 +0200

    add contexts to tests

--- spec/core/domains/ip_domain_spec.rb
+++ spec/core/domains/ip_domain_spec.rb
@@ -4,21 +4,33 @@ describe IpDomain do
   valid_ip_address = '192.168.0.1'
   invalid_ip_address = '192.168.0.260'
 
-  it 'creates IP domain object' do
-    ip = IpDomain.new(valid_ip_address)
+  describe '#initialize' do
+    context 'when provided with a string' do
+      it 'creates IP domain object' do
+        ip = IpDomain.new(valid_ip_address)
 
-    expect(ip.address).to eq valid_ip_address
+        expect(ip.address).to eq valid_ip_address
+      end
+
+    end
   end
 
-  it 'validates valid IP address' do
-    ip = IpDomain.new(valid_ip_address)
+  describe '#valid?' do
+    context 'when IP domain object is created with a valid IP address' do
+      it 'validates it' do
+        ip = IpDomain.new(valid_ip_address)
 
-    expect(ip.valid?).to be_truthy
-  end
+        expect(ip.valid?).to be_truthy
+      end
 
-  it 'does not validate invalid IP address' do
-    ip = IpDomain.new(invalid_ip_address)
+    end
 
-    expect(ip.valid?).to be_falsey
+    context 'when IP domain object is created with a invalid IP address' do
+      it 'does not validate it' do
+        ip = IpDomain.new(invalid_ip_address)
+
+        expect(ip.valid?).to be_falsey
+      end
+    end
   end
 end
--- spec/core/domains/uri_domain_spec.rb
+++ spec/core/domains/uri_domain_spec.rb
@@ -5,27 +5,40 @@ describe UriDomain do
   empty_path = ''
   invalid_path = '/   index'
 
-  it 'creates URI domain object' do
-    uri = UriDomain.new(valid_path)
+  describe '#initialize' do
+    context 'when provided with a string' do
+      it 'creates URI domain object' do
+        uri = UriDomain.new(valid_path)
 
-    expect(uri.path).to eq valid_path
+        expect(uri.path).to eq valid_path
+      end
+
+    end
   end
 
-  it 'validates valid URI' do
-    uri = UriDomain.new(valid_path)
+  describe '#valid?' do
+    context 'when IP domain object is created with a valid URI' do
+      it 'validates it' do
+        uri = UriDomain.new(valid_path)
 
-    expect(uri.valid?).to be_truthy
-  end
+        expect(uri.valid?).to be_truthy
+      end
+    end
 
-  it 'validates empty URI' do
-    uri = UriDomain.new(empty_path)
+    context 'when IP domain object is created with an empty URI' do
+      it 'validates it' do
+        uri = UriDomain.new(empty_path)
 
-    expect(uri.valid?).to be_truthy
-  end
+        expect(uri.valid?).to be_truthy
+      end
+    end
 
-  it 'does not validate invalid URI' do
-    uri = UriDomain.new(invalid_path)
+    context 'when IP domain object is created with an invalid URI' do
+      it 'does not validate it' do
+        uri = UriDomain.new(invalid_path)
 
-    expect(uri.valid?).to be_falsey
+        expect(uri.valid?).to be_falsey
+      end
+    end
   end
 end
