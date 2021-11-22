bec721716a261b2ed2c2a1a91e6d5b6f57d6eb00
Anton Matusenco <anton@matusenco.com>
2021-11-22 04:10:27 +0200

    aestetic fix in test

--- spec/external_interfaces/cli_spec.rb
+++ spec/external_interfaces/cli_spec.rb
@@ -6,12 +6,12 @@ describe ExternalInterfaces::Cli do
     context 'when called with parse_webserver_log argument' do
       it 'executes ParseWebserverLog and returns from it' do
         allow_any_instance_of(UseCases::ParseWebserverLog).
-          to receive(:execute).and_return([['one', 1], ['two', 2]])
+          to receive(:execute).and_return([{ 'one' => 1 }, { 'two' => 2 }])
 
         result = ExternalInterfaces::Cli.new.
           execute_use_case(%w[parse_webserver_log arg1 arg2])
 
-        expect(result).to eq(['two', 2]) # last array element
+        expect(result).to eq({ 'two' => 2 }) # last array element
       end
     end
   end
